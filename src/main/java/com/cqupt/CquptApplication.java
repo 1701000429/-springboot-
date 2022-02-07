d47cee49ef83eb24cbd86f1b58b5406c9902dce7
waitingfree <2235113906@qq.com>
2022-02-07 19:03:44 +0800

    springboot初始化

--- src/main/java/com/cqupt/CquptApplication.java
+++ src/main/java/com/cqupt/CquptApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CquptApplication {
 
     public static void main(String[] args) {
+        System.out.println("Springboot初始化======");
         SpringApplication.run(CquptApplication.class, args);
     }
 
